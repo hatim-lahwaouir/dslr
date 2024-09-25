1fee7ab4479e06080c1b9e952999710dd2358efe
Hatim Lahwaouir <hlahwaou@e1r2p14.1337.ma>
2024-09-25 06:19:08 +0100

    still going

--- src/describe.py
+++ src/describe.py
@@ -3,6 +3,8 @@ import sys
 from csv import DictReader 
 
 
+MAX_ALINMENT = 0
+
 
 def is_float(string):
     try:
@@ -12,18 +14,65 @@ def is_float(string):
         return False
 
 def read_csv(csv_file:str) -> dict:
+    global MAX_ALINMENT
     data = {}
+    feature_not_num = set()
     with open(csv_file, 'r')  as file:
-        reader = DictReader(file)    
+        reader = DictReader(file)
         for row in reader:
             for feature in row:
-                if row[feature]
-                
+                MAX_ALINMENT = max(MAX_ALINMENT, len(feature))
+                if feature in feature_not_num:
+                    continue
+                try:
+                    if len(row[feature]) == 0:
+                        continue
+                    float(row[feature])
+                    MAX_ALINMENT = max(MAX_ALINMENT, len(row[feature]))
+                except:
+                    feature_not_num.add(feature)
+                    continue
+                value = np.float64(row[feature])
+                data[feature] = np.append(data.get(feature, np.empty(1)) , np.array([value]))
+
+
+    return data
+
+
+def printMesurement(m,  vect):
+    if m == 'Count':
+        print(str(vect.size).ljust(MAX_ALINMENT), end='')
+
+
+
+
+def printData(data):
+    list_of_features = []
+    mesurements = ['Count', 'Mean', 'Std', 'Min', '25%' , '50%', '75%', 'Max']
+
+    for i, feature in enumerate(data):
+        if len(list_of_features) == 4 or len(data) == i + 1:
+            print(MAX_ALINMENT * ' ', end='')
+            for f in list_of_features:
+                print(f.ljust(MAX_ALINMENT) + '  ', end='')
+            print('')
+            for m in mesurements:
+                print(m.ljust(MAX_ALINMENT) + ' ', end='')
+
+                for f in list_of_features:
+                    printMesurement(m, data[f])
+                print('')
+            print('')
+            list_of_features = []
+        list_of_features.append(feature)
+
+
 def main(argv:list[str]):
     if len(argv) != 2:
         print("Invalid arguments")
         exit(1)
-    read_csv(argv[1])
+    data = read_csv(argv[1])
+    printData(data)
     
 
 
